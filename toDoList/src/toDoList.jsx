890edcfca70994ad80c47b9b7f69e0094490c9c7
Bengi <114860608+Egemen-Bengi@users.noreply.github.com>
2025-04-15 16:19:27 +0300

    Update toDoList.jsx
    react

---
+++ toDoList/src/toDoList.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+function toDoList(){
+    const [toDo, setToDo] = useState([])
+    const [newToDo, setNewToDo] = useState("")
+
+    function onChangeInput(event){
+
+    }
+
+    function addToDo(){
+
+    }
+
+    function deleteToDo(index){
+
+    }
+}
+
+export default toDoList
